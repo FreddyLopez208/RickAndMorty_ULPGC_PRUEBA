65085243f1f8fc4191fdc12436e12ee2275a177c
Freddy Lopez <freddy.lopez102@alu.ulpgc.es>
2023-12-10 00:41:22 +0000

    Initial Commit

---
+++ RickAndMorty_ULPGC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty_ULPGC
+//
+//  Created by Freddy Alexander López Gutiérrez on 10/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
